3d4c0b5e891dab750eccf3dc658896da3a2b73d4
Ahron <Ahron@10.146.193.20>
2025-05-20 10:28:30 -0300

    Teste V 0.02

--- src/main/java/principal/controleDeEstoque.java
+++ src/main/java/principal/controleDeEstoque.java
@@ -4,6 +4,6 @@ import modelo.Categoria;
 public class controleDeEstoque {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Salve gurizada, so de boa? Hello World!");
+        JOptionPane.showMessageDialog(null, "Salve rapaziada, so de boa? Hello World!");
     }
 }
