e13a49bdb49ac5ad21b8a18998e62a9d067276aa
gabri <gabri@10.146.192.193>
2025-05-20 10:20:28 -0300

    Teste V0.01

--- src/main/java/principal/controleDeEstoque.java
+++ src/main/java/principal/controleDeEstoque.java
@@ -1,8 +1,9 @@
 package principal;
+import javax.swing.JOptionPane;
 import modelo.Categoria;
 public class controleDeEstoque {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JOptionPane.showMessageDialog(null, "Salve gurizada, so de boa? Hello World!");
     }
 }
