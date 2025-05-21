ed67c616150feaed5f05bbfe8863b5a308c6b6e1
gabri <gabri@Acer>
2025-05-20 21:58:53 -0300

    Teste v0.04

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,9 +1,9 @@
 package principal;
 import javax.swing.JOptionPane;
 import modelo.Categoria;
-public class controleDeEstoque {
+public class Principal {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, " so de boa? Hello World! Testezin");
+        JOptionPane.showMessageDialog(null, "Testando a main, v0.04");
     }
 }
