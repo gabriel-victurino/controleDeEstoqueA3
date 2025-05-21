0c67eac9b0574a790808d6e5d1808ed9ff051f0d
feeh <feeh@DESKTOP-2FBFR4I>
2025-05-20 21:06:17 -0300

    Teste v0.05

--- src/main/java/principal/controleDeEstoque.java
+++ src/main/java/principal/controleDeEstoque.java
@@ -4,6 +4,6 @@ import modelo.Categoria;
 public class controleDeEstoque {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, " so de boa? Hello World! Testezin");
+        JOptionPane.showMessageDialog(null, " so de boa? Hello World! Testezin1");
     }
 }
