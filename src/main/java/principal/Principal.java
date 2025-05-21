c47d2c3c8c63f52d0a1f3df2a8dbe155625ef1c8
gabri <gabri@Acer>
2025-05-20 21:35:02 -0300

    Teste

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,9 +1,9 @@
 package principal;
 import javax.swing.JOptionPane;
 import modelo.Categoria;
-public class controleDeEstoque {
+public class Principal {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, " so de boa? Hello World! Testezin1");
+        JOptionPane.showMessageDialog(null, " TOC CANSADO DE TESTAR E DAR ERRADO");
     }
 }
