80bf07ae1f3197c8a460a201a9782ae020433b47
gabri <gabri@Acer>
2025-05-20 22:10:24 -0300

    Teste v0.05

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -4,6 +4,6 @@ import modelo.Categoria;
 public class Principal {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Testando a main, v0.04");
+        JOptionPane.showMessageDialog(null, "Testando a main, v0.05"); // não aguento maiss
     }
 }
