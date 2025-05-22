4d155e39c9e38e951024baa2045c372a7e3c5fd1
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-05-22 15:39:14 -0300

    Testando commit v0.06

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -4,7 +4,7 @@ import modelo.Categoria;
 public class Principal {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Teste de autor, v0.06"); // não aguento maiss
+        JOptionPane.showMessageDialog(null, "Teste de autor, v0.06"); // não aguento maiss teste
 
     }
 }
