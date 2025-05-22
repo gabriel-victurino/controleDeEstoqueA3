26b371816c32e886ba54ad400b1a38801b850fe8
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-05-22 16:53:54 -0300

    Adicionado package DAO e visao.

--- src/main/java/modelo/SistemaEstoque.java
+++ src/main/java/modelo/SistemaEstoque.java
@@ -1,5 +1,61 @@
 package modelo;
+import java.util.Date;
 
 public class SistemaEstoque {
-    int i = 2;
+    private int id;
+    private int idProduto;
+    private Date data;
+    private String operacao; // Entrada ou saida
+    private int quantidade;
+
+    public SistemaEstoque() {
+    }
+
+    public SistemaEstoque(int id, int idProduto, Date data, String operacao, int quantidade) {
+        this.id = id;
+        this.idProduto = id;
+        this.data = data;
+        this.operacao = operacao;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getOperacao() {
+        return operacao;
+    }
+
+    public void setOperacao(String operacao) {
+        this.operacao = operacao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
--- src/main/java/visao/FrmGerenciaEstoque.java
+++ src/main/java/visao/FrmGerenciaEstoque.java
@@ -6,14 +6,14 @@ package visao;
 
 /**
  *
- * @author 10725114655
+ * @author gabri
  */
-public class semnome extends javax.swing.JFrame {
+public class FrmGerenciaEstoque extends javax.swing.JFrame {
 
     /**
-     * Creates new form semnome
+     * Creates new form FrmGerenciaEstoque
      */
-    public semnome() {
+    public FrmGerenciaEstoque() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class semnome extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(semnome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmGerenciaEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(semnome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmGerenciaEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(semnome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmGerenciaEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(semnome.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FrmGerenciaEstoque.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new semnome().setVisible(true);
+                new FrmGerenciaEstoque().setVisible(true);
             }
         });
     }
