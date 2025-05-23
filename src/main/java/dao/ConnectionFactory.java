00c044eb5187c2b28e687c17ce2dcccf89b7affd
Ahron <AhronStoco@gmail.com>
2025-05-23 06:55:54 -0300

    Conexão com o Banco de Dados - v0.10

--- src/main/java/dao/ConnectionFactory.java
+++ src/main/java/dao/ConnectionFactory.java
@@ -1,4 +1,22 @@
 package dao;
-
+import java.sql.Connection;
+//conecta com o banco
+import java.sql.DriverManager;
+//abre a conexão
+import java.sql.SQLException;
+//trata possíveis erros
+                        
 public class ConnectionFactory {  
+    private static final String URL = "jdbc:mysql://localhost:3306/nosso_banco";
+    private static final String USUARIO = "usuario";
+    private static final String SENHA = "nossa_senha";
+    //conexão com as informações do banco de dados, depois tem que substituir quando criado
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA); //cria conexão com os dados
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao conectar ao banco de dados: " + e.getMessage()); //caso der erro
+        }
+    } 
 }
