1f9bf01479756cb8ffa8a621db0ad2eac98ce479
adhrianom <adhrianocanal@gmail.com>
2025-05-23 16:39:29 -0300

    Alterações nas classes da package modelo e correções nas classes da package DAO. v0.16

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
     public static void main(String[] args) {
      // Instancia a interface gráfica;
     FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
-    // Torna a janela visível;
+    // Torna a janela visível
     objetotela.setVisible(true);
     }
 }
