244713a6c47d119aa7eab514223c3fc967e7286d
Ahron <AhronStoco@gmail.com>
2025-05-23 07:44:34 -0300

    Classe ProdutoDAO iniciada - v0.11

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -11,16 +11,48 @@ public class ProdutoDAO {
     public ProdutoDAO(Connection conexao) {
         this.conexao = conexao;
     }
-    
-    public void inserir(Produto produto) {
-      // Requer SQL (Ahron)
+//incompleta só pra começar, falta preço, estoque, quant minima, quant maxima e categoria (fazer Produto.java primeiro)
+    public void inserir(Produto produto) { 
+        String sql = "INSERT INTO produtos (nome, unidade) VALUES (?,?)";
+        
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setString(1, produto.getNome());
+            stmt.setString(2, produto.getUnidade());
+            stmt.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.err.println("Erro ao inserir produto " + e.getMessage());
+        }
     }
     
-    public void atualizar(Produto produto) {
-        // Requer SQL (Ahron)
+    public void alterar(Produto produto) {
+        String sql = "UPDATE produtos SET nome = ?, unidade = ? WHERE id = ?";
+        
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getQuantidadeMaxima());
+            stmt.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.err.println("Erro ao alterar produto " + e.getMessage());
+        }
     }
     
-    public void deletar(int id) {
-        // Requer SQL (Ahron)
+    public void apagar(int id) {
+        String sql = "DELETE FROM produtos WHERE id = ?";
+        
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.err.println("Erro ao remover produto " + e.getMessage());
+        }
     }
 }
