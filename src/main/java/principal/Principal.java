978b25a2f96423d71e2c9ff77c75bbac35c877db
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-05-23 09:04:30 -0300

    Organização v0.09

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -4,7 +4,7 @@ import visao.FrmMenuPrincipal;
 public class Principal {
 
     public static void main(String[] args) {
-        // Instancia a interface gráfica
+     // Instancia a interface gráfica
     FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
     // Torna a janela visível
     objetotela.setVisible(true);
